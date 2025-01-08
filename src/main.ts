c3678c8a51a5d3e8ffbb6e8333aae19b19f5941e
MFella <mfella@wp.pl>
2025-01-08 19:53:30 +0100

    Update cors option in main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ let serverHandler: Handler;
 
 async function bootstrap(): Promise<Handler> {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   await app.init();
